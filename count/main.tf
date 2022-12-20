4a1a1062c7002e14d1fa7c3df78347c296521e1b
llllsssswwww <nicekid33@naver.com>
2022-12-20 16:22:24 +0900

    20221220

---
+++ count/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "user_names" {
+  description = "Create IAM users with these names"
+  type        = list(string)
+  default    = ["aws01-neo", "aws01-trinity", "aws01-morpheous" ]
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name = var.user_names[count.index]
+}
+
+output "neo_arn" {
+  value = aws_iam_user.example[*].arn
+}
